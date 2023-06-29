76ec7bc276e2d075581ed27cedc37135d7f71eb8
bartai5 <6mutaibartai58@gmail.com>
2023-06-29 11:45:35 +0300

    scroll navivation

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function textLoad(){
 
 textLoad();
 setInterval(textLoad, 12100);
+
+// The Actice selector in list to indicate the ka small line under the link name
+let list = document.querySelectorAll(".list-item");
+for (let i=0; i<list.length; i++){
+    list[i].onclick = function(){
+        let j = 0;
+        while(j < list.length){
+            list[j++].className = 'list-item';
+        }
+        list[i].className = "list-item active";
+        console.log("active");
+    }
+}
